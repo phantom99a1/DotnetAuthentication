7c7a7e361aa333b8cbc3be8f984b280e8aeb3b8d
phantom99a1 <viendanho99a1@gmail.com>
2025-02-02 23:52:43 +0700

    Add some files, entities

---
+++ WebUI/Domain/Entities/ApplicationUser.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace WebUI.Domain.Entities
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Gender { get; set; } = string.Empty;
+        public string? RefreshToken { get; set; }
+        public DateTime? RefreshTokenExpireTime { get; set; }
+        public DateTime? CreateDateTime { get; set; }
+        public DateTime? LastModifiedDateTime { get; set; }
+    }
+}
--- WebUI/obj/Debug/net8.0/WebUI.AssemblyInfo.cs
+++ WebUI/obj/Debug/net8.0/WebUI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b98701ec61f57e36f00bcdf38f8425f36604565")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
